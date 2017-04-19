c9606b491acb5872b455f10c9f89e74ab049d2c6
Ted John <ted@brambles.org>
2017-04-20 00:38:11 +0100

    Increment version to 0.1.2

--- src/tterm/Properties/AssemblyInfo.cs
+++ src/tterm/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly
 )]
 
-[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.1.2.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
